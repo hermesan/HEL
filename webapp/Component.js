a0d7e2339d9805c064ea15e14bda23f88cfd2abf
hermesan <hermesan@gmx.de>
2018-11-03 17:59:25 +0100

    Update 5
    Signed-off-by: hermesan <hermesan@gmx.de>

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,15 +1,18 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-], function (UIComponent, JSONModel) {
+    "Well/controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("Well.Component", {
         metadata: {
             manifest: "json"
         },
         init: function () {
+            
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+
             // set data model
             var oData = {
                 recipient: {
@@ -18,6 +21,22 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
-        }
+
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+        },
+
+        exit: function () {
+            //destroy function
+            this._helloDialog.destroy();
+            //free Webbrowser memory
+            delete this._helloDialog;
+        },
+
+        openHelloDialog: function () {
+            //Aufruf
+            this._helloDialog.open();
+        }    
+
     });
 });
